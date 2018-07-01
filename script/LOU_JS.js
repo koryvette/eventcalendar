f3d342fb3fdebfc650a6cecf9e4f52653061563b
Kory Howell <korydhowell@yahoo.com>
2018-07-01 01:35:13 -0400

    background loop

--- script/LOU_JS.js
+++ script/LOU_JS.js
@@ -1,19 +1,52 @@
 
-var imgs = new Array("img/papajohn_night_revised.jpg",
+/* var imgs = new Array("img/papajohn_night_revised.jpg",
                       "img/papajohn_revised.jpg",
                       "img/nightbridge_revised.jpg",
                       "img/redthunder_revised.jpg",
                       "img/walkingbridge_revised.jpg",
+                      "img/4thstreet_revised.jpg",
                       "img/skyline_day.jpg");
 
  function changeBg() {
      var imgUrl = imgs[Math.floor(Math.random()*imgs.length)];
      $('#banner').css('background-image', 'url(' + imgUrl + ')');
-     $('#banner').fadeIn(1000); //this is new, will fade in smoothly
+     $('#banner').fadeIn(400); //this is new, will fade in smoothly
  }
 
  function changeBackgroundSmoothly() {
-     $('#banner').fadeOut(1000, changeBg); //this is new, will fade out smoothly
+     $('#banner').fadeOut(400, changeBg); //this is new, will fade out smoothly
  }
 
  setInterval(changeBackgroundSmoothly,5000);
+
+ */
+
+ $(document).ready(function(){
+ var count = 0;
+ var images = ["img/papajohn_night_revised.jpg",
+               "img/skyline_day.jpg",
+               "img/nightbridge_revised.jpg",
+               "img/papajohn_revised.jpg",
+               "img/redthunder_revised.jpg",
+               "img/4thstreet_revised.jpg",
+               "img/walkingbridge_revised.jpg",
+               "img/FLstate_revised.jpg",
+               "img/thunder_1_revised.jpg",
+               "img/YUM_revised.jpg"];
+
+ var image = $('#banner');
+
+ image.css("background-image","url("+images[count]+")");
+
+ setInterval(function(){
+   image.fadeOut(500, function(){
+     image.css("background-image","url("+images[count++]+")");
+     image.fadeIn(500);
+   });
+   if(count == images.length)
+     {
+       count = 0;
+     }
+ },5000);
+
+ });
