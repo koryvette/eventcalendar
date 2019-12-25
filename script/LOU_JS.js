839ca158440ce4bb291fa7f15ceec04bc1c6dec6
Kory Howell <korydhowell@yahoo.com>
2019-12-25 12:20:17 -0500

    pagination and carousel

--- script/LOU_JS.js
+++ script/LOU_JS.js
@@ -108,3 +108,76 @@ $(document).ready(function(){
           });
      }
 });
+
+//  Pagination  //
+var numberOfItems = $('#tileLayout .event-thumb').length;
+var limitPerPage = 50;
+$("#tileLayout .event-thumb:gt(" + (limitPerPage - 1) + ")").hide();
+var totalPages = Math.round(numberOfItems/limitPerPage);
+$('.pagination').append("<li class='page-item current-page active'><a class='page-link' href='javascript:void(0)'>" + 1 + "</a></li>");
+
+for (var i = 2; i <= totalPages; i++){
+     $('.pagination').append("<li class='page-item current-page'><a class='page-link' href='javascript:void(0)'>" + i + "</a></li>");
+}
+
+$(".pagination").append("<li class='page-item' id='next-page'><a class='page-link' href='javascript:void(0)'>Next</a></li>");
+
+$(".pagination li.current-page").on("click", function(){
+     var currentPage = $(this).index();
+     $(".pagination li").removeClass("active");
+     $(this).addClass("active");
+     $("#tileLayout .event-thumb").hide();
+
+     var grandTotal = limitPerPage * currentPage;
+
+     for (var i  = grandTotal - limitPerPage; i < grandTotal; i++){
+          $("#tileLayout .event-thumb:eq(" + i + ")").show();
+     }
+});
+
+$("#next-page").on("click", function()
+{
+     var currentPage = $(".pagination li.active").index();
+     if (currentPage === totalPages) 
+     {
+          return false;
+     }
+          else
+          {
+          currentPage++;
+          $(".pagination li").removeClass("active");
+          $("#tileLayout .event-thumb").hide();
+
+          var grandTotal = limitPerPage * currentPage;
+
+          for (var i  = grandTotal - limitPerPage; i < grandTotal; i++) 
+               {
+                    $("#tileLayout .event-thumb:eq(" + i + ")").show();
+               }
+               $(".pagination li.current-page:eq(" + (currentPage - 1) + ")").addClass("active");
+          }
+});
+
+$("#prev-page").on("click", function()
+{
+     var currentPage = $(".pagination li.active").index();
+     if (currentPage === 1) 
+     {
+          return false;
+     }
+          else
+          {
+          currentPage--;
+          $(".pagination li").removeClass("active");
+          $("#tileLayout .event-thumb").hide();
+
+          var grandTotal = limitPerPage * currentPage;
+
+          for (var i  = grandTotal - limitPerPage; i < grandTotal; i++) 
+               {
+                    $("#tileLayout .event-thumb:eq(" + i + ")").show();
+               }
+               $(".pagination li.current-page:eq(" + (currentPage - 1) + ")").addClass("active");
+          }
+});
+
